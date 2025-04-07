9aec491a83999c4a5a61395a8eddae062ce84bd8
antonioGohz <a.d.j.gogeascoecheahernandez@utwente.nl>
2025-04-07 14:17:07 +0200

    Fix remove duplicates and adds plot results

--- decomposition_lib/remduplicates.py
+++ decomposition_lib/remduplicates.py
@@ -38,49 +38,51 @@ def remduplicates(PulseT, distime, distime2, maxlag, jitter, tol, fsamp, sil=Non
     idsnewVec, distimenew, Pulsenew = [], [], []
     idsnew = list(range(MUn))
 
-    for i in tqdm(range(MUn), desc="Removing duplicates"):
-        if not distimmp:
-            break
-        
-        comdis = np.zeros(len(distimmp))
-        for j in range(1, len(distimmp)-1):
-            corr = correlate(firings[0, :], firings[j, :], mode='valid')
-            correl_max = np.max(corr)
-            lag_idx = np.argmax(corr) - maxlag
-            distimetemp = np.array(distimmp[j]) + lag_idx if correl_max > 0.2 else distimmp[j]
-            common = np.intersect1d(distimmp[0], distimetemp)
-            comdis[j] = len(common) / max(len(distime[0]), len(distime[j]))
+    i = 0
+    with tqdm(total=MUn, desc="Removing duplicates") as pbar:
+        while distimmp:
 
-        duplicates = [0] + [j for j in range(1, len(comdis)) if comdis[j] >= tol]
+            comdis = np.zeros(len(distimmp))
+            for j in range(1, len(distimmp)):
+                corr = correlate(firings[0, :], firings[j, :], mode='valid')
+                correl_max = np.max(corr)
+                lag_idx = np.argmax(corr) - maxlag
+                distimetemp = np.array(distimmp[j]) + lag_idx if correl_max > 0.2 else distimmp[j]
+                common = np.intersect1d(distimmp[0], distimetemp)
+                comdis[j] = len(common) / max(len(distime[0]), len(distime[j]))
 
-        if len(duplicates) > 1:
-            active_period = np.array([(distime[d][-1] - distime[d][0]) / fsamp for d in duplicates])
-            ranks1 = np.argsort(np.argsort(active_period))
-            
-            if sil is None:
-                isi_metric = [np.std(np.diff(distime[d])) / np.mean(np.diff(distime[d])) for d in duplicates]
-                ranks2 = np.argsort(np.argsort(isi_metric))
+            duplicates = [0] + [j for j in range(len(comdis)) if comdis[j] >= tol ] # and j != i
+
+            if len(duplicates) > 1:
+                active_period = np.array([(distime[d][-1] - distime[d][0]) / fsamp for d in duplicates])
+                ranks1 = np.argsort(np.argsort(active_period))
+                
+                if sil is None:
+                    isi_metric = [np.std(np.diff(distime[d])) / np.mean(np.diff(distime[d])) for d in duplicates]
+                    ranks2 = np.argsort(np.argsort(isi_metric))
+                else:
+                    ranks2 = np.argsort(sil[duplicates])
+                    sil = [sil[d] for d in range(len(sil)) if d not in duplicates]
+
+                ranks = ranks1 * np.mean(np.diff(active_period)) / np.mean(active_period) + \
+                        ranks2 * np.mean(np.diff(isi_metric)) / np.mean(isi_metric)
+                survivor = duplicates[np.argmax(ranks)]
             else:
-                ranks2 = np.argsort(sil[duplicates])
-                sil = [sil[d] for d in range(len(sil)) if d not in duplicates]
+                survivor = duplicates[0]
 
-            ranks = ranks1 * np.mean(np.diff(active_period)) / np.mean(active_period) + \
-                    ranks2 * np.mean(np.diff(isi_metric)) / np.mean(isi_metric)
-            survivor = duplicates[np.argmax(ranks)]
-        else:
-            survivor = duplicates[0]
+            idsnewVec.append(idsnew[survivor])
+            distimenew.append(distime[survivor])
+            Pulsenew.append(PulseT[survivor, :])
 
-        idsnewVec.append(idsnew[survivor])
-        distimenew.append(distime[survivor])
-        Pulsenew.append(PulseT[survivor, :])
+            # Remove duplicates
+            for idx in sorted(duplicates, reverse=True):
+                del distime[idx]
+                del distime2[idx]
+                del distimmp[idx]
+                del idsnew[idx]
+                PulseT = np.delete(PulseT, idx, axis=0)
+                firings = np.delete(firings, idx, axis=0)
 
-        # Remove duplicates
-        for idx in sorted(duplicates, reverse=True):
-            del distime[idx]
-            del distime2[idx]
-            del distimmp[idx]
-            del idsnew[idx]
-            PulseT = np.delete(PulseT, idx, axis=0)
-            firings = np.delete(firings, idx, axis=0)
+            pbar.update(1)
 
     return np.array(Pulsenew), distimenew, idsnewVec
